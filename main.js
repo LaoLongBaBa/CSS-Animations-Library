261f3b427da295ec7f277ce2a8c8ede4319a7185
LaoLongBaBa <laolonggod@gmail.com>
2022-04-23 22:45:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,64 @@
+var main = document.querySelector('#Animate');
+
+function A1(t){
+    main.removeAttribute('class');
+    main.classList.add('changeColor')
+}
+function A2(t){
+    main.removeAttribute('class');
+    main.classList.add('fontSize')
+}
+function A3(t){
+    main.removeAttribute('class');
+    main.classList.add('Jump')
+}
+function A4(t){
+    main.removeAttribute('class');
+    main.classList.add('rotate-center')
+}
+function A5(t){
+    main.removeAttribute('class');
+    main.classList.add('flip-horizontal')
+}
+function A6(t){
+    main.removeAttribute('class');
+    main.classList.add('swing')
+}
+function A7(t){
+    main.removeAttribute('class');
+    main.classList.add('slide')
+}
+function A8(t){
+    main.removeAttribute('class');
+    main.classList.add('shadow')
+}
+function A9(t){
+    main.removeAttribute('class');
+    main.classList.add('tracking')
+}
+function A10(t){
+    main.removeAttribute('class');
+    main.classList.add('focus-in')
+}
+function A11(t){
+    main.removeAttribute('class');
+    main.classList.add('shake')
+}
+function A12(t){
+    main.removeAttribute('class');
+    main.classList.add('jello')
+}
+function A13(t){
+    main.removeAttribute('class');
+    main.classList.add('flip-out')
+}
+function A14(t){
+    main.removeAttribute('class');
+    main.classList.add('roll-in')
+}
+function A15(t){
+    main.removeAttribute('class');
+    main.classList.add('fade-in')
+}
+
+
